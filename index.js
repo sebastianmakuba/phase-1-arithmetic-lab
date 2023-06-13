d6e64d8c4670f44d606e9ee0980e7943d76b42f7
+ <+>
2023-06-13 20:12:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 20
+const num4= 8
+const mod = 20 % 8
+
+const set = [20, 3, 6, 1, 18, 16]
+const max = Math.max(...set)
